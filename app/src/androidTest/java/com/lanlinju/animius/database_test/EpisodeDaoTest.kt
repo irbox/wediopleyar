3dd70311bfe3cd9c79868beb34288177fcbfb3c3
Sir Irbox <189630825+irbox@users.noreply.github.com>
2025-05-08 21:05:58 +0530

    Update EpisodeDaoTest.kt

--- app/src/androidTest/java/com/lanlinju/animius/database_test/EpisodeDaoTest.kt
+++ app/src/androidTest/java/com/lanlinju/animius/database_test/EpisodeDaoTest.kt
@@ -25,7 +25,7 @@ class EpisodeDaoTest {
     private lateinit var historyDao: HistoryDao
     private lateinit var animeDatabase: AnimeDatabase
 
-    private var history1 = HistoryEntity(1, "海贼王1", "img1", "/video1", SourceMode.Yhdm.name)
+    private var history1 = HistoryEntity(1, "One Piece 1", "img1", "/video1", SourceMode.Yhdm.name)
 
     @Before
     fun createDb() {
@@ -49,8 +49,8 @@ class EpisodeDaoTest {
     fun daoInsert_insertsEpisodeIntoDB() = runBlocking {
         addOneHistoryToDb()
         val history = historyDao.getHistory("/video1").first()
-        val ep1 = EpisodeEntity(1, history.historyId, "第一集", "/video2")
-        val ep2 = EpisodeEntity(2, history.historyId, "第二集", "/video3")
+        val ep1 = EpisodeEntity(1, history.historyId, "Episode 1", "/video2")
+        val ep2 = EpisodeEntity(2, history.historyId, "Episode 2", "/video3")
 
         episodeDao.insertEpisodes(listOf(ep1, ep2))
         val episodes = episodeDao.getEpisodes(history.historyId).first()
@@ -92,15 +92,15 @@ class EpisodeDaoTest {
     private suspend fun addOneEpisodesToDb() {
         addOneHistoryToDb()
         val history = historyDao.getHistory("/video1").first()
-        val ep1 = EpisodeEntity(1, history.historyId, "第一集", "/video2")
+        val ep1 = EpisodeEntity(1, history.historyId, "Episode 1", "/video2")
         episodeDao.insertEpisode(ep1)
     }
 
     private suspend fun addTwoEpisodesToDb() {
         addOneHistoryToDb()
         val history = historyDao.getHistory("/video1").first()
-        val ep1 = EpisodeEntity(1, history.historyId, "第一集", "/video2")
-        val ep2 = EpisodeEntity(2, history.historyId, "第二集", "/video3")
+        val ep1 = EpisodeEntity(1, history.historyId, "Episode 1", "/video2")
+        val ep2 = EpisodeEntity(2, history.historyId, "Episode 2", "/video3")
         episodeDao.insertEpisodes(listOf(ep1, ep2))
     }
 
